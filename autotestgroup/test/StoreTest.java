b4e5d72ad7d31ee9e1e29814d9faea17fd7008aa
nerga <aagren86@gmail.com>
2019-04-03 07:12:12 +0200

    First commit

---
+++ autotestgroup/test/StoreTest.java
@@ -0,0 +1,19 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StoreTest {
+
+    @Test
+    public void learnResponse() {
+        //These are test that are gonna be here.
+    }
+
+    @Test
+    public void getResponse() {
+    }
+
+    @Test
+    public void unlearnAll() {
+    }
+}
